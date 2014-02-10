ef2c2f45e567d4c2ad50fae684ca9d726e032c07
krontogiannis <rodoconst@hotmail.com>
2014-02-10 15:24:26 +0200

    Fixed missing CloudLinq.Base project.

--- src/CloudLinq.Base/Properties/AssemblyInfo.cs
+++ src/CloudLinq.Base/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5ccebc84-9289-419c-86ed-cdc166271c02")]
+[assembly: Guid("a0611e57-772b-4528-8bcf-2ed1bbac3ce3")]
 
 // Version information for an assembly consists of the following four values:
 //
